51d69fbae81f69d663c2c1144c53ba8b759d0a59
TalkingF <140305263+TalkingF@users.noreply.github.com>
2026-01-27 12:56:44 +1030

    main file setup

--- Shox/Shox.cs
+++ Shox/Shox.cs
@@ -2,8 +2,82 @@
 
 class Shox
 {
-    static void Main(string[] args)
+
+    private static bool _hadError = false;
+    
+    // Main entry point
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length > 1)
+        {
+            Console.Error.WriteLine("Usage: Shox [Script]");
+            Environment.Exit(1);
+        }
+
+        if (args.Length == 1)
+        {
+            Run(args[0]);
+        }
+
+        else
+        {
+            RunAsRepl();
+        }
+    }
+
+    // Runs file if present
+    private static void RunFile(string path)
+    {
+        if (Path.Exists(path))
+        {
+            var contents = File.ReadAllBytes(path);
+
+            var code = contents.ToString();
+           
+            if (code is null) return;
+            
+            Run(code);
+            
+            if (_hadError)
+            {
+                Environment.Exit(1);
+            }
+
+
+        }
+    }
+    
+    //Run program as a REPL, interprets line by line
+    private static void RunAsRepl()
+    {
+        
+        Console.Write("> ");
+        while ((Console.ReadLine()) is { } line)
+        {
+            Run(line);
+            _hadError = false;
+            Console.Write("> ");
+            
+        }
+        
+    }
+    
+    private static void Run(string contents)
+    {
+        
+    }
+
+
+
+    public static void Error(int line, String message)
+    {
+        
+    }
+
+    private static void Report(int line, string where, string message)
+    {
+        Console.Error.WriteLine("[line: " + line + "] Error " + where + ": " +
+                                message);
+        _hadError = true;
     }
 }
