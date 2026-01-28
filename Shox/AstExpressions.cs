cb02c647c27265caa022d22803812a9b7f1ed83c
TalkingF <140305263+TalkingF@users.noreply.github.com>
2026-01-29 10:19:42 +1030

    ast expressions

---
+++ Shox/AstExpressions.cs
@@ -0,0 +1,11 @@
+﻿namespace Shox;
+
+public abstract record Expr;
+
+public record Binary(Expr Left, Token Op, Expr Right) : Expr;
+
+public record Grouping(Expr Expression) : Expr;
+
+public record Unary(Token Op, Expr Right) : Expr;
+
+public record Literal(object Value) : Expr;
