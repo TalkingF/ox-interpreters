ef452ffb10cd9d6ed44dc5ba40d17189986f9e54
TalkingF <140305263+TalkingF@users.noreply.github.com>
2026-01-30 12:21:36 +1030

    Expression generation

--- Shox/AstExpressions.cs
+++ Shox/AstExpressions.cs
@@ -1,11 +1,46 @@
-﻿namespace Shox;
+namespace Shox;
 
-public abstract record Expr;
+public abstract record Expr
+{
+    public abstract T Accept<T>(IVisitor<T> visitor);
+}
 
-public record Binary(Expr Left, Token Op, Expr Right) : Expr;
+public interface IVisitor<out T>
+{
+    T VisitBinaryExpr(Binary expr);
+    T VisitGroupingExpr(Grouping expr);
+    T VisitLiteralExpr(Literal expr);
+    T VisitUnaryExpr(Unary expr);
+}
 
-public record Grouping(Expr Expression) : Expr;
+public record Binary(Expr Left, Token Op, Expr Right) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor)
+    {
+        return visitor.VisitBinaryExpr(this);
+    }
+}
 
-public record Unary(Token Op, Expr Right) : Expr;
+public record Grouping(Expr Expression) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor)
+    {
+        return visitor.VisitGroupingExpr(this);
+    }
+}
 
-public record Literal(object Value) : Expr;
+public record Literal(object Value) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor)
+    {
+        return visitor.VisitLiteralExpr(this);
+    }
+}
+
+public record Unary(Token Op, Expr Right) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor)
+    {
+        return visitor.VisitUnaryExpr(this);
+    }
+}
---
+++ Shox/Tool/GenerateAstExpressions.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace Shox.Tool;
+
+public static class GenerateAstExpressions
+
+{
+    private const string ExpressionPath = "Tool/ast-gen.txt";
+    private const string OutputPath = "AstExpressions.cs";
+
+    //Generates a csharp file with all the records and overrides for configured
+    //ast expressions. For best effect its recommended that you format the output.
+    public static void Generate()
+    {
+        const string initial = """
+                               namespace Shox;
+                               public abstract record Expr
+                               {
+                               public abstract T Accept<T>(IVisitor<T> visitor);
+                               }
+
+                               public interface IVisitor<out T>
+                               {
+
+                               """;
+
+        var writeText = new StringBuilder(initial);
+
+        var data = GetTypes();
+
+        //generate visitor interfaces
+        foreach (var tup in data)
+            writeText.AppendLine("T " + "Visit" + tup.Item1 + "Expr(" +
+                                 tup.Item1 + " expr);");
+
+        writeText.AppendLine("}");
+
+        //generate records
+        foreach (var tup in data)
+        {
+            //record class gen
+            writeText.Append("public record " + tup.Item1 + "(");
+
+            foreach (var (name, type) in tup.Item2)
+                writeText.Append(type + " " + name + ", ");
+            writeText.Remove(writeText.Length - 2, 2);
+
+            writeText.AppendLine("): Expr");
+
+            //override accept function
+            writeText.AppendLine("{");
+
+            writeText.AppendLine(
+                "public override T Accept<T>(IVisitor<T> visitor) {");
+            writeText.AppendLine("return visitor.Visit" + tup.Item1 +
+                                 "Expr(this);");
+            writeText.AppendLine("}");
+            writeText.AppendLine("}");
+
+            File.WriteAllText(OutputPath, writeText.ToString());
+        }
+    }
+
+
+    //opens the ast expressions file and creates a list. Each entry in the list comprises
+    //a tuple representing the expression name and a dictionary mapping name of the var to type.
+    private static List<(string, Dictionary<string, string>)> GetTypes()
+    {
+        var data = new List<(string, Dictionary<string, string>)>();
+        var lines = File.ReadLines(ExpressionPath);
+
+
+        foreach (var line in lines)
+        {
+            var dict = new Dictionary<string, string>();
+
+            var content = line.Split(' ');
+
+            for (var i = 1; i < content.Length; i += 2)
+                //e.g. Binary: Left
+                dict[content[i + 1]] = content[i];
+
+            var tup = (content[0], dict);
+            data.Add(tup);
+        }
+
+        return data;
+    }
+}
